a0414a25fea2740278bb1f3cf2ad27f823dbe0aa
Bernardo Barreto <bernardo.barreto@ua.pt>
2022-06-21 01:23:25 +0100

    update payments_page

---
+++ Payments/Local/backend/apis/version1/route_payment_page.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+import requests
+
+templates = Jinja2Templates(directory="templates")
+router = APIRouter()
+
+
+@router.post("/")
+async def home(request: Request):
+	body = await request.body()
+	body = body.decode("utf-8")
+
+	body_list=body.split('&')
+	body_list.pop(0)
+	# print(body)
+
+	token = body_list[0].split('=')[1]
+	amount = body_list[1].split('=')[1]
+	username = body_list[2].split('=')[1]
+	# print(token)
+	# print(amount)
+	# print(username)
+	return templates.TemplateResponse("general_pages/homepage.html",{"request":request})
