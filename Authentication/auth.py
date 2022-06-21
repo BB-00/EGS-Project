3d2a9f3a12489b8d63b50133c4e1536cea81d8c5
79AFonso <apteixeira@ua.pt>
2022-06-21 02:08:30 +0100

    update

--- Authentication/auth.py
+++ Authentication/auth.py
@@ -2,6 +2,7 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta
+from fastapi import HTTPException
 from db import DbHandler
 import json
 
